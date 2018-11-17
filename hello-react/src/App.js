390b8da7249b85396aafe62bce2e72dc565a4258
Shin Changseon <skywing27@gmail.com>
2018-11-17 23:37:28 +0900

    exercise: chapter04 added chapter04(event handling) exercise code

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import MyComponent from './MyComponent'
+import EventPractice from './EventPractice'
 
 class App extends Component {
   render() {
     return (
-      <MyComponent />
+      // <MyComponent />
+      <EventPractice />
     )
   }
 }
