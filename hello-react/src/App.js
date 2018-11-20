fbf2c373e084e6ea5dd0010edd1eb280d624c3f2
Shin Changseon <skywing27@gmail.com>
2018-11-20 22:39:48 +0900

    exercise: chapter07(LifeCycle Method of Component) added chapter07(LifeCycle Method of Component) exercise code

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,8 +5,23 @@ import EventTest from './EventTest'
 import ValidationSample from './ValidationSample'
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample'
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
+
   render() {
     return (
       // <MyComponent />
@@ -19,7 +34,12 @@ class App extends Component {
       //   <ScrollBox ref={(ref) => this.scrollBox = ref} />
       //   <button onClick={() => {this.scrollBox.scrollToBottom()}}>맨 밑으로 스크롤</button>
       // </div>
-      <IterationSample />
+      // <IterationSample />
+      <div>
+        <button onClick = {this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color = {this.state.color} />
+      </div>
+      
     )
   }
 }
