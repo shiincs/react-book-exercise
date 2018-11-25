03794354d20383c87f76ce0ca17b62085642dd85
Shin Changseon <skywing27@gmail.com>
2018-11-25 17:40:45 +0900

    style: TodoList All created all components and drew all components and styled all components but now just static app not dynamic app

--- todo-list/config/webpack.config.dev.js
+++ todo-list/config/webpack.config.dev.js
@@ -44,7 +44,7 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
     require.resolve('style-loader'),
     {
       loader: require.resolve('css-loader'),
-      options: cssOptions,
+      options: cssOptions
     },
     {
       // Options for PostCSS as we reference these options twice
@@ -66,12 +66,6 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
         ],
       },
     },
-    {
-      loader: require.resolve('sass-loader'),
-      options: {
-        includePaths: [paths.styles]
-      }
-    }
   ];
   if (preProcessor) {
     loaders.push(require.resolve(preProcessor));
@@ -293,8 +287,8 @@ module.exports = {
             use: getStyleLoaders({
               importLoaders: 1,
               modules: true,
-              // getLocalIdent: getCSSModuleLocalIdent,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]'
+              getLocalIdent: getCSSModuleLocalIdent,
+              // localIdentName: '[path][name]__[local]--[hash:base64:5]'
             }),
           },
           // Opt-in support for SASS (using .scss or .sass extensions).
@@ -305,7 +299,45 @@ module.exports = {
           {
             test: sassRegex,
             exclude: sassModuleRegex,
-            use: getStyleLoaders({ importLoaders: 2 }, 'sass-loader'),
+            // use: getStyleLoaders({ importLoaders: 2 }, 'sass-loader'),
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  // cssOptions,
+                  importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]'
+                }
+              },
+              {
+                // Options for PostCSS as we reference these options twice
+                // Adds vendor prefixing based on your specified browser support in
+                // package.json
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebook/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    require('postcss-preset-env')({
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    }),
+                  ],
+                },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  includePaths: [paths.styles]
+                }
+              }
+            ]
           },
           // Adds support for CSS Modules, but using SASS
           // using the extension .module.scss or .module.sass
---
+++ todo-list/src/components/TodoInput/TodoInput.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import styles from './TodoInput.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
+// Functional Component
+const TodoInput = ({value, onChange, onInsert}) => {
+
+  // Enter 키가 눌리면 onInsert() 메소드를 실행한다.
+  const handleKeyPress = (e) => {
+    e.key === 'Enter' && onInsert()
+  }
+
+  return (
+    <div className = {cx('todo-input')}>
+      <input onChange = {onChange} value = {value} onKeyPress = {handleKeyPress} />
+      <div className = {cx('add-button')} onClick = {onInsert} >추가</div>
+    </div>
+  )
+}
+
+export default TodoInput
